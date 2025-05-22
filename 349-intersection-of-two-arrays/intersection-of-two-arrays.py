58e8101990d9ea31393802c9561f243d573a667a
Vasavi <89859886+PhoneixProgrammer@users.noreply.github.com>
2025-05-21 21:39:51 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (43.58%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.py
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.py
@@ -1,8 +1,14 @@
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        '''
         res=set()
         for item in nums1:
             if item in nums2:
                 res.add(item)
         return list(res)
-#time complexity : O(n*m) 
+        '''
+#time complexity : O(n*m) which is not a optimal solution
+
+        return list(set(nums1) & set(nums2))
+        #Optimal Solution
+        
