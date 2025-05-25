ad130daf2a26a34756583a340bd7f4f8a6b15a29
Vasavi <89859886+PhoneixProgrammer@users.noreply.github.com>
2025-05-24 22:14:09 -0400

    Time: 0 ms (100.00%) | Memory: 19.1 MB (28.88%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.py
+++ 414-third-maximum-number/third-maximum-number.py
@@ -3,12 +3,14 @@ class Solution:
         unique_nums=[]
         seen=set()
 
-        for num in nums :
+        for num in nums :#O(n)  time
             if num not in seen:
                 seen.add(num)
                 unique_nums.append(num)
-        unique_nums = sorted(unique_nums, reverse=True)
-        if len(unique_nums)>2:
+        unique_nums = sorted(unique_nums, reverse=True) #O(nlogn)
+        if len(unique_nums)>2: #O(1)
             return unique_nums[2]
         else:
             return max(unique_nums)
+
+        #t.c : O(nlogn)
