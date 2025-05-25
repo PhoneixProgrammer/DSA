e74c5727e1d383dfe0744ba1919ebd4acb0830b0
Vasavi <89859886+PhoneixProgrammer@users.noreply.github.com>
2025-05-24 22:43:22 -0400

    Time: 0 ms (100.00%) | Memory: 18.4 MB (88.96%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.py
+++ 414-third-maximum-number/third-maximum-number.py
@@ -1,8 +1,8 @@
 class Solution:
     def thirdMax(self, nums: List[int]) -> int:
+        '''
         unique_nums=[]
         seen=set()
-
         for num in nums :#O(n)  time
             if num not in seen:
                 seen.add(num)
@@ -14,3 +14,30 @@ class Solution:
             return max(unique_nums)
 
         #t.c : O(nlogn)
+        '''
+
+        #Optimal Solution : O(n)
+        first = second = third = None
+
+        for num in nums :
+            if num ==  first or num == second or num == third :
+                continue
+            
+            if first is None or num > first :
+                third = second
+                second = first
+                first = num 
+            
+            elif second is None or num > second :
+                third = second
+                second = num
+            
+            elif third is None or num > third :
+                third = num
+        
+        if third is not None :
+            return third
+        else:
+            return first
+
+        #t.c : O(n)
