e4a0628bfd8d93a3cc5dacd69ba31dbd8f7599f4
Vasavi <89859886+PhoneixProgrammer@users.noreply.github.com>
2025-05-29 18:24:11 -0400

    Time: 10 ms (75.05%) | Memory: 18 MB (48.94%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,25 +1,33 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
         s_dic = {}
         t_dic = {}
 
-        for x in s:
+        for x in s: 
+            #s_dic[x] = s_dic.get(x, 0) + 1
             if x in s_dic:
                 s_dic[x] += 1
             else:
                 s_dic[x] = 1
-
-        #print(s_dic)
-        
+     
         for y in t:
+            #t_dic[y] = t_dic.get(y,0)+1
             if y in t_dic:
                 t_dic[y] += 1
             else : 
                 t_dic[y]=1
         
-        #print(t_dic)
-        
+        #return s_dic == t_dic
         if s_dic == t_dic :
             return True
         else :
             return False
+
+        #t.c :O(m+n) , s.c:O(m+n)
+
+        # Alternatively we can use Counter for generating hashmaps automatically for s and t
+        # and check both
+        # from collections import Counter
+        # return Counter(s) == Counter(t)
