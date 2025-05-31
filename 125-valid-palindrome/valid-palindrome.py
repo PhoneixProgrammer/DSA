35025a2e3af54d067c8eb1f4ea5547100aaa88a1
Vasavi <89859886+PhoneixProgrammer@users.noreply.github.com>
2025-05-31 00:02:55 -0400

    Time: 3 ms (98.93%) | Memory: 19.3 MB (25.57%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s=re.sub(r'[^a-zA-Z0-9]','',s).lower()
+        
+        #2 pointers
+        left, right = 0, len(s)-1
+
+        while left < right :
+            if s[left] != s[right]:
+                return False
+            left += 1
+            right -=1
+        
+        return True
