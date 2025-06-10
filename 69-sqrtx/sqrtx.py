4028eda1f1b329d27d371ba7e185e7eee0db76dd
Vasavi <89859886+PhoneixProgrammer@users.noreply.github.com>
2025-06-10 19:30:53 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (50.98%) - LeetSync

---
+++ 69-sqrtx/sqrtx.py
@@ -0,0 +1,18 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x < 2 :
+            return x # as sqrt (0) =0 and sqrt(1)=1
+        
+        left = 1
+        right =  x//2 # no need to check beyond this point as sqrt is usually  half of the number
+
+        while left<=right :
+            mid = (left+right)//2
+            if mid*mid == x : #condition for perfect square
+                return mid
+            elif mid*mid < x:
+                left = mid + 1 #go right to find a bigger candidate
+            else:
+                right = mid -1 #go left to find a smaller candidate
+        #when the loop ends, right is the integer part of sqrt(x)
+        return right
