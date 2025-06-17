f9ac805120710f601aa34833d32a7d5ab5434d2f
Vasavi <89859886+PhoneixProgrammer@users.noreply.github.com>
2025-06-17 01:14:24 -0400

    Time: 4 ms (21.46%) | Memory: 17.8 MB (47.21%) - LeetSync

---
+++ 3723-sum-of-good-numbers/sum-of-good-numbers.py
@@ -0,0 +1,27 @@
+class Solution:
+    def sumOfGoodNumbers(self, nums: List[int], k: int) -> int:
+        
+        good_num = []
+        n = len(nums)
+
+        for i in range(n):
+            left_exists = (i - k) >= 0
+            right_exists = (i + k) < n
+
+            if left_exists and right_exists:
+                if nums[i] > nums[i - k] and nums[i] > nums[i + k]:
+                    good_num.append(nums[i])
+            elif left_exists:
+                if nums[i] > nums[i - k]:
+                    good_num.append(nums[i])
+            elif right_exists:
+                if nums[i] > nums[i + k]:
+                    good_num.append(nums[i])
+            else:
+                # Neither index exists → nums[i] is good
+                good_num.append(nums[i])
+        
+        res=0
+        for i in range(len(good_num)):
+            res = res + good_num[i]
+        return res
