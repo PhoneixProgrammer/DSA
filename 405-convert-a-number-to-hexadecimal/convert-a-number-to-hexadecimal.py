4f1a7f6034a9131c00d4951f093c2f2410d89414
Vasavi <89859886+PhoneixProgrammer@users.noreply.github.com>
2025-06-23 23:56:13 -0400

    Time: 0 ms (100.00%) | Memory: 17.6 MB (86.93%) - LeetSync

---
+++ 405-convert-a-number-to-hexadecimal/convert-a-number-to-hexadecimal.py
@@ -0,0 +1,16 @@
+class Solution:
+    def toHex(self, num: int) -> str:
+        if num == 0:
+            return "0"
+        
+        hex_chars = "0123456789abcdef"
+        result = ""
+
+        if num < 0 :
+            num += 2 ** 32
+
+        while num > 0:
+            reminder = num % 16 
+            result = hex_chars[reminder] +result
+            num //= 16
+        return result
