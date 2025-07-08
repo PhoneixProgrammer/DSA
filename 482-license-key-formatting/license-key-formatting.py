8a94823f53550a0a8abde1be8266e023e6e18509
Vasavi <89859886+PhoneixProgrammer@users.noreply.github.com>
2025-07-08 00:57:12 -0400

    Time: 6 ms (73.44%) | Memory: 18.9 MB (33.20%) - LeetSync

---
+++ 482-license-key-formatting/license-key-formatting.py
@@ -0,0 +1,13 @@
+class Solution:
+    def licenseKeyFormatting(self, s: str, k: int) -> str:
+        s = s.replace("-", "").upper()
+        result = []
+        count = 0
+        for  i in range(len(s)-1,-1,-1):
+            result.append(s[i])
+            count += 1
+            if count == k and i!=0:
+                result.append('-')
+                count = 0
+        
+        return ''.join(reversed(result))
