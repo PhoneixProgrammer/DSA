1394a005ececdb63dccd898b70b26b3dcdf077ab
Vasavi <89859886+PhoneixProgrammer@users.noreply.github.com>
2025-07-11 02:23:34 -0400

    Time: 129 ms (33.66%) | Memory: 18.2 MB (54.70%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -0,0 +1,33 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if not s or not t :
+            return ""
+
+        need = Counter(t)
+        window = {}
+        have = 0
+        need_count = len(need)
+
+        res = ""
+        res_len = float("inf")
+        left = 0
+
+        for right in range(len(s)):
+            char = s[right]
+            window[char] = window.get(char,0) +1
+
+            if char in need and window[char] == need[char] :
+                have += 1
+            
+            while have == need_count:
+                #Update Result
+                if (right - left + 1) < res_len :
+                    res = s[left:right + 1]
+                    res_len = right - left + 1 
+
+                #Pop from the left of the window
+                window[s[left]] -= 1
+                if s[left] in need and window[s[left]] < need[s[left]]:
+                    have -= 1
+                left += 1
+        return res
