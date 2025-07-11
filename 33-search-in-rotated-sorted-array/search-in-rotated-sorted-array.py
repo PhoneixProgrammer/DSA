b9112b52e0634e1be03204cdc6e56a648f5a4d33
Vasavi <89859886+PhoneixProgrammer@users.noreply.github.com>
2025-07-11 02:56:05 -0400

    Time: 0 ms (100.00%) | Memory: 18.1 MB (41.60%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,9 +1,34 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        if target in nums : 
-            return nums.index(target)
-        else :
+        #O(logn) solution
+        if not nums :
             return -1
 
-#Time Complexity : O(n)
-# Space Complexity : O(1)
+        left, right =  0, len(nums)-1
+
+        #Find pivot (smallest element index)
+        while left < right :
+            mid = (left+right)//2 
+            if nums[mid] > nums[right]:
+                left = mid + 1
+            else :
+                right = mid
+        pivot = left
+
+        #Decide which half to binary search
+        left, right = 0, len(nums)-1
+        if target >= nums[pivot] and target <= nums[right]:
+            left = pivot
+        else : 
+            right = pivot -1 
+
+        #standard binary search 
+        while left <= right :
+            mid =(left+right)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target :
+                left = mid +1
+            else:
+                right = mid-1
+        return -1
