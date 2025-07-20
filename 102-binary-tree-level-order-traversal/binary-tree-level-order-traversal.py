c27563a6eaa44c3afe314e8151b9b03772937dcd
Vasavi Bondapalli <89859886+PhoneixProgrammer@users.noreply.github.com>
2025-07-20 19:29:47 -0400

    Time: 0 ms (100.00%) | Memory: 18.4 MB (59.64%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
@@ -9,21 +9,25 @@ class Solution:
         if not root : 
             return []
 
-        result =[]
-        queue = deque([root])
+        result =[] #final answer : list of lists
+        queue = deque([root]) # initialize queu with root node for BFS
 
         while queue :
-            level_size = len(queue)
-            level_nodes = []
+            level_size = len(queue) # Number of nodes at the current level
+            level_nodes = [] #list to hold current level's node values
 
             for _ in range(level_size):
-                node =  queue.popleft()
+                node =  queue.popleft() #pop from the front of the queue
                 level_nodes.append(node.val)
 
+                #enqueue left and right children (next level)
                 if node.left :
                     queue.append(node.left)
                 if node.right :
                     queue.append(node.right)
 
-            result.append(level_nodes)
+            result.append(level_nodes) #append this level results 
         return result
+
+        #Time Complexity : O(n)
+        #Space Complexity : O(n) (in worst case each node is stored level by level)
