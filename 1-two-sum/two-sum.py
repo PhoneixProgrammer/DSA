2beb29c2431666ba98933f6ae3e1c74785f588ad
Vasavi Bondapalli <89859886+PhoneixProgrammer@users.noreply.github.com>
2025-07-31 15:36:37 -0400

    Time: 3 ms (50.27%) | Memory: 18.7 MB (61.66%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,10 +1,20 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        num_map = {}
+        dic_map = {}
+
         for i, num in enumerate(nums):
-            complement = target - num
-            if complement in num_map:
-                return [num_map[complement], i]
-            num_map[num] = i
-            #Data structure :hashmap- O(1) 
+            complement = target-num
+            if complement in dic_map :
+                return [dic_map[complement],i]
+            dic_map[num] = i
+
+        '''Time Complexity :
+        1. i traverse the nums ,each digit is visited so if there are n didgits it will take O(n) time
+        2. each lookup and insertion in dictionary is O(1) time
         
+        so total average :  O(n)
+        '''
+
+        '''Space Complexity :
+        in worst case, you may store all n elements in dictionary so O(n)
+        '''
