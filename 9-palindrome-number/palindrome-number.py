0b24162dfec7bc55848eaae368fd430eed07944f
Vasavi Bondapalli <89859886+PhoneixProgrammer@users.noreply.github.com>
2025-07-31 19:17:33 -0400

    Time: 3 ms (88.34%) | Memory: 17.8 MB (64.18%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -1,5 +1,16 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
+        # Negative numbers and numbers ending with 0
+        if x < 0  or (x%10 == 0 and x!=0):
+            return False
+
+        reversed_half = 0
+        while x > reversed_half : 
+            digit = x%10 
+            reversed_half = reversed_half*10 + digit
+            x = x // 10
+        return x == reversed_half or x == reversed_half//10
+        '''
 
         x= str(x)
         left = 0
@@ -14,3 +25,4 @@ class Solution:
 
         # Time Complexity : O(n)
         #Space Complexity : O(n)
+        '''
