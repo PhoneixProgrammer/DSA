20a71b5d7be082b1660031a75d11de78825fece8
Vasavi Bondapalli <89859886+PhoneixProgrammer@users.noreply.github.com>
2025-07-31 23:43:43 -0400

    Time: 2 ms (83.20%) | Memory: 17.9 MB (40.00%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,6 +1,19 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
         roman_dict = {"I":1,"V":5,"X" :10,"L": 50, "C": 100,"D" :500,"M":1000}
+        total = 0
+        prev = 0
+        for i in range(len(s)-1,-1,-1 ) :
+            ch = s[i]
+            val = roman_dict[ch]
+            if val < prev :
+                total -= val
+            else:
+                total+=val
+            prev = val
+        return total
+        '''
+        roman_dict = {"I":1,"V":5,"X" :10,"L": 50, "C": 100,"D" :500,"M":1000}
         total = 0 
         prev = 0
         for ch in reversed(s):
@@ -13,3 +26,4 @@ class Solution:
         return total
         #Time Complexity : o(n)
         #Space Complexity : O(1)
+        '''
