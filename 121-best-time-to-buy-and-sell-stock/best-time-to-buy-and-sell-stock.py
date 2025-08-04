3da8cab7394f1bf0f7bce46283cad0c4cedb933f
Vasavi Bondapalli <89859886+PhoneixProgrammer@users.noreply.github.com>
2025-08-03 20:46:28 -0400

    Time: 37 ms (82.28%) | Memory: 27 MB (32.21%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,27 +1,11 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        '''
-        # Brute force Approach :  passed 199/212 test cases
-        # time limit exceeded
-        max_value = 0 
+        min_price= float('inf')
+        max_profit=0
+
         for i in range(len(prices)):
-            for j in range(i+1, len(prices)):
-                if prices[j] > prices[i] :
-                    max_value = max(max_value,prices[j]-prices[i])
-        return max_value
-        '''
-
-        min_price =  float('inf') #Tracks the lowest price seen so far
-        max_profit = 0 # track the maximum profit found
-
-        for price in prices : 
-            if price < min_price :
-                min_price = price # updates the min_price if the current_price is lower
-            else : 
-                #calculate profit if sold today and update max_profit if better
-                max_profit = max(max_profit, price - min_price)
+            if prices[i] < min_price :
+                min_price = prices[i]
+            elif prices[i] - min_price > max_profit: 
+                max_profit = prices[i] - min_price
         return max_profit
-
-        #time complexity : O(n) as only one time traversal through the prices array
-        # space complecity : O(1)
-            
