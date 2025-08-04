38846f2372d9cc2d6c657dd8e51f5224747200d7
Vasavi Bondapalli <89859886+PhoneixProgrammer@users.noreply.github.com>
2025-08-03 22:56:12 -0400

    Time: 11 ms (34.81%) | Memory: 23.1 MB (19.20%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,33 +1,13 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        '''
-        s=re.sub(r'[^a-zA-Z0-9]','',s).lower()
-        
-        #2 pointers
-        left, right = 0, len(s)-1
+        s = ''.join(c.lower() for c in s if c.isalnum())
+        left_p = 0
+        right_p = len(s)-1
 
-        while left < right :
-            if s[left] != s[right]:
+        while left_p < right_p :
+            if s[left_p] == s[right_p]:
+                left_p+=1
+                right_p -= 1
+            else :
                 return False
-            left += 1
-            right -=1
-        
-        return True
-
-        #t.c : O(n) and s.c : O(n)
-        '''
-        left, right = 0, len(s)-1
-
-        while left < right :
-
-            while left < right and not s[left].isalnum():
-                left += 1
-            while left < right and not s[right].isalnum():
-                right -= 1
-            
-            if s[left].lower() != s[right].lower():
-                return False
-            left += 1
-            right -= 1
-
         return True
