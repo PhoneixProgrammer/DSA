48d9255bb81f6911bfce04cfa34514940c9577ca
Vasavi Bondapalli <89859886+PhoneixProgrammer@users.noreply.github.com>
2025-08-05 17:28:43 -0400

    Time: 27 ms (81.34%) | Memory: 36.7 MB (50.95%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.py
+++ 219-contains-duplicate-ii/contains-duplicate-ii.py
@@ -1,24 +1,12 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        '''
-        for i in range(len(nums)):
-            for j in range(i+1,len(nums)) :
-                if (nums[i]==nums[j] and abs(i-j)<=k):
-                    return True
-        return False
+        # DS - hashmap 
+        seen = {}
 
-        #time limit exceeded
-        '''
-        # Hashmap DS
-        seen ={}
-        for i,num in enumerate(nums): # this takes O(n) time
-            if num in seen : # O(1) because hashmap
-                if i-seen[num]<=k : #O(1)
+        for i in range(len(nums)) :
+            if nums[i] in seen :
+                if abs(i-seen[nums[i]]) <= k:
                     return True
-            seen[num]=i #O(1)
-            ##print(seen)
+            
+            seen[nums[i]]=i
         return False
-        # Time complexity : O(n)
-        #Space Complexity : O(n)
-        # because in worst case scenario if all the elements in the nums is distinct then it stores each element in the seen which is a dict
-        # so it stores each value with its index , if you have 1 billion elements in the array , its stores 1 billion value to its index value
