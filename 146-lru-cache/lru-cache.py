bea47c9054d30c93d96e29658fa7b6f9e8634683
Vasavi Bondapalli <89859886+PhoneixProgrammer@users.noreply.github.com>
2025-09-18 18:21:48 -0400

    Time: 108 ms (87.55%) | Memory: 78 MB (43.29%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -2,30 +2,22 @@ class LRUCache:
 
     def __init__(self, capacity: int):
         self.cache = OrderedDict()
-        self.capacity = capacity 
+        self.capacity = capacity
 
     def get(self, key: int) -> int:
-        if key not in self.cache :
-            return -1
+        if key not in self.cache : 
+            return -1 
         self.cache.move_to_end(key)
         return self.cache[key]
 
     def put(self, key: int, value: int) -> None:
-        if key in self.cache : 
-            #Update value and mark as recently used
+        if key in self.cache :
             self.cache.move_to_end(key)
-        self.cache[key] = value
+        self.cache[key] = value 
         if len(self.cache) > self.capacity :
-            #pop the least recently used(first item)
             self.cache.popitem(last= False)
-
-
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
 # obj.put(key,value)
-
-'''
-
-'''
