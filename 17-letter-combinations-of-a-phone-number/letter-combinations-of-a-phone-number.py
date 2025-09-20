7d8095bb54ca83d3f633c43feda6a4def67b5d13
Vasavi Bondapalli <89859886+PhoneixProgrammer@users.noreply.github.com>
2025-09-20 16:58:33 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (23.55%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,8 +1,7 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         res= []
-        if len(digits)==0:
-            return res
+   
 
         numbers_map = {"1" : "", "2": "abc","3":"def","4":"ghi","5":"jkl","6":"mno","7":"pqrs","8":"tuv","9":"wxyz"}
 
@@ -12,9 +11,17 @@ class Solution:
                 res.append(curStr)
                 return 
             for c in numbers_map[digits[i]]:
-                backtrack(i+1, curStr + c)
+                backtrack(i+1, curStr + c) #1,a
 
         if digits : 
             backtrack(0,"")
 
         return res        
+
+        '''
+        Summary Table
+Aspect	Complexity
+Time	O(n * 3^m * 4^k) → worst-case O(n * 4^n)
+Space	O(n * 3^m * 4^k) → dominated by output
+Recursion depth	O(n)
+'''
