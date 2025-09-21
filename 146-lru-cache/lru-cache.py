a0e5335946c11bfd77b2c16df3c77842424937a1
Vasavi Bondapalli <89859886+PhoneixProgrammer@users.noreply.github.com>
2025-09-21 12:28:08 -0400

    Time: 146 ms (48.97%) | Memory: 78 MB (43.22%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,25 +1,50 @@
+class Node:
+    def __init__(self, key: int, value: int):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+
+
 class LRUCache:
-
     def __init__(self, capacity: int):
-        self.cache = OrderedDict()
         self.capacity = capacity
+        self.cache = {}
 
+        # Dummy head & tail
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def _remove(self, node: Node):
+        prev, nxt = node.prev, node.next
+        prev.next = nxt
+        nxt.prev = prev
+
+    def _add(self, node: Node):
+        prev, nxt = self.tail.prev, self.tail
+        prev.next = node
+        node.prev = prev
+        node.next = nxt
+        nxt.prev = node
 
     def get(self, key: int) -> int:
-        if key not in self.cache :
+        if key not in self.cache:
             return -1
-        self.cache.move_to_end(key)
-        return self.cache[key]
+        node = self.cache[key]
+        self._remove(node)
+        self._add(node)
+        return node.value
 
     def put(self, key: int, value: int) -> None:
-        if key in self.cache : 
-            self.cache.move_to_end(key)
-        self.cache[key] = value 
-        if len(self.cache) >  self.capacity :
-            self.cache.popitem(last = False) 
+        if key in self.cache:
+            self._remove(self.cache[key])
+        node = Node(key, value)
+        self._add(node)
+        self.cache[key] = node
 
-
-# Your LRUCache object will be instantiated and called as such:
-# obj = LRUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
+        if len(self.cache) > self.capacity:
+            lru = self.head.next
+            self._remove(lru)
+            del self.cache[lru.key]
