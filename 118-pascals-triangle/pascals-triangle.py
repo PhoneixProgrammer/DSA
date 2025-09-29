12645bb0b22d808e3e259934d4443dc400f769d5
Vasavi Bondapalli <89859886+PhoneixProgrammer@users.noreply.github.com>
2025-09-28 20:56:58 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (37.45%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -1,20 +1,19 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        triangle=[]
-        for row in range(numRows):
-            if row == 0:
-                triangle.append([1])
-            else : 
-                # prev_row takes the last row that has been added to the triangle
-                # because to build the current row because each number depends on the 2 numbers above it 
-                prev_row = triangle[-1]
-                current_row = [1]
+        if numRows == 0:
+            return []
+        
+        triangle = [[1]]
 
-                for i in range(1,len(prev_row)):
-                    current_row.append(prev_row[i-1]+prev_row[i])
-                current_row.append(1)
-                triangle.append(current_row)
+        for i in range(1,numRows) :
+            prev_row = triangle[i-1]
+
+            row = [1]
+            
+            for j in range(1,i):
+                row.append(prev_row[j-1]+prev_row[j])
+            row.append(1)
+            triangle.append(row)
         return triangle
 
-        # Solution 1:TimeComplexity is O(n2); n-->numRows
-            
+        #time and space : O(numRow^2)
