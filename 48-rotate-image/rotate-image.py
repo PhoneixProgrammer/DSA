55e2bd032d6fb47d2bca7073af1735934f5d978c
Vasavi Bondapalli <89859886+PhoneixProgrammer@users.noreply.github.com>
2025-10-14 09:16:59 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (41.44%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -5,11 +5,18 @@ class Solution:
         """
         n = len(matrix)
 
-        #STEP 1: TRANSPOSE OF MATRIX( SWIPE ACROSS DIAGONAL)
-        for i in range(n):
-            for j in range(i+1,n):
-                matrix[i][j],matrix[j][i] = matrix[j][i],matrix[i][j]
+        #  1    2    3             1  4  7       7  4  1
+        #  4    5    6      ---->  2  5  8  -->  8  5  2
+        #  7    8     9            3  6   9      9  6  3
+
+        # Transpose of the matrix -- > O(n^2)
+        for i in range(n): #i = 0
+            for j in range(i+1,n) : 
+                matrix[i][j], matrix[j][i] = matrix[j][i],matrix[i][j]
         
-        #STEP 2: REVERSE EACH ROW
-        for row in matrix:
+        for row in matrix : # O(n^2)
             row.reverse()
+        
+        #Optimal Time Complexity :
+        #Total Time complexity : O(n^2)
+
