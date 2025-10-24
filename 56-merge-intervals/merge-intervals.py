57590a4a74589ed6f2f281c30957e51b4eb77e05
Vasavi Bondapalli <89859886+PhoneixProgrammer@users.noreply.github.com>
2025-10-23 20:19:27 -0400

    Time: 0 ms (100.00%) | Memory: 21.5 MB (38.00%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -10,3 +10,12 @@ class Solution:
             else :
                 merged[-1][1] = max(merged[-1][1],end)
         return merged 
+        '''
+        | Step                   | Time Complexity | Space Complexity |
+| ---------------------- | --------------- | ---------------- |
+| Sort intervals         | O(n log n)      | O(log n)         |
+| Merge loop             | O(n)            | O(1)             |
+| Store merged intervals | —               | O(n)             |
+| **Total**              | O(n log n)      | O(n)             |
+
+'''
