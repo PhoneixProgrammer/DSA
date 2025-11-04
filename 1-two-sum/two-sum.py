30bc17b311b9ccbe7a6b93764c76b7a72d4ff046
Vasavi Bondapalli <89859886+PhoneixProgrammer@users.noreply.github.com>
2025-11-04 18:14:26 -0500

    Time: 4 ms (41.09%) | Memory: 19.1 MB (10.37%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,13 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        #datastructiure
-        dict_map = {}
-
+        nums_dict = {}
         for i in range(len(nums)):
             complement = target - nums[i]
-
-            if complement in dict_map :
-                return [dict_map[complement],i] 
-            dict_map[nums[i]] = i
-  
-
+            if complement in nums_dict:
+                return [i, nums_dict[complement]]
+            nums_dict[nums[i]] = i
