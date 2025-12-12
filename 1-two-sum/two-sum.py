bec9f13e64b20a1f7f03aa41be6b520610686f4d
Vasavi Bondapalli <89859886+PhoneixProgrammer@users.noreply.github.com>
2025-12-11 22:34:26 -0500

    Time: 3 ms (49.62%) | Memory: 19 MB (24.01%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -4,15 +4,10 @@ class Solution:
         nums_dict = {}
 
         for i in range(len(nums)):
-
             complement = target - nums[i]
-
             if complement in nums_dict:
-
                 return [i, nums_dict[complement]]
-
             nums_dict[nums[i]] = i
 
     #Time Complexity : O(n)
-    
     #Space Complexity : O(n)
